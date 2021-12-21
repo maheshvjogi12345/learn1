12aa10d57732f5de827cb76fc8e4571ab65c3e82
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-21 11:02:52 +0530

    learning numbers

---
+++ Numbers.java
@@ -0,0 +1,19 @@
+class Numbers{
+	public static void main(String[] args){
+		byte []ids = {22,21,20,19,17};			
+		System.out.println(ids);
+		System.out.println(ids[01]);
+		System.out.println(ids[02]);
+	
+	}
+}
+
+
+
+
+
+
+
+		
+	
+	
